89b92af29be7153079935250edeadb7a7eaa2290
FrancoDavid <franco.avalos@live.com>
2025-01-11 20:16:13 -0300

    feat: directives defined

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MainLayout } from './Layout/main.layout';
 import { AppComponent } from './app.component';
+import { HomePage } from './Pages/home.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    MainLayout
   ],
   imports: [
     BrowserModule,
